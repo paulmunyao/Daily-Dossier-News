b93c9d9738303490a763bd07923a05fec2dea91a
Paul Munyao <paulmunyao094@gmail.com>
2022-02-28 02:25:20 +0300

    Fetching data to be dispalyed

---
+++ app/utils.py
@@ -0,0 +1,11 @@
+import requests
+from decouple import config
+
+NEWS_API_KEY = config('NEWS_API_KEY')
+COUNTRY = "in"
+
+
+def get_latest_news():
+    news_data = requests.get(
+        f"https:newsapi.org/v2/top-headlines?country={COUNTRY}&apiKey={NEWS_API_KEY}").json()
+    return news_data["articles"]
