e71c5288ebc026405aa7fca8c10db0e559d3173c
Paul Munyao <paulmunyao094@gmail.com>
2022-02-27 05:05:05 +0300

    Add an introductory sentence

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,14 @@
 from flask import render_template
 from app import app
 
+
 @app.route('/')
 @app.route('/index')
 def index():
-     user = {"username": "There"}
-     return render_template("index.html",title="Home",user=user)
-     '''
+    user = {"username": "There"}
+    post = {"Welcome to the dossier <br> a one stop place where we believe <br> information should be available to everyone everytime at no cost"}
+    return render_template("index.html", title="Home", user=user,post=post)
+    '''
 <html >
     <head >
         <title > Home-Page - Daily Dossier < /title >
