c6131f5caacf00f340e34f8e2240657319de24ee
Paul Munyao <paulmunyao094@gmail.com>
2022-02-27 11:58:12 +0300

    declare a custom error handling of 500

--- app/errors.py
+++ app/errors.py
@@ -4,3 +4,7 @@ from app import app, db
 @app.errorhandler(404)
 def not_found_error(error):
     return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def not_found_error(error):
+    return render_template('500.html'), 500    
