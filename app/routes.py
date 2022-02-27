b081bf83269fa53bcf95bb53bfac1936cd4df82b
Paul Munyao <paulmunyao094@gmail.com>
2022-02-27 03:16:17 +0300

    Create a mock object of what is to be diaplyed in the HTML page

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,16 @@
 from app import app
 
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Daily Dossier"
+    user = {"username": "There"}
+    return '''
+<html >
+    <head >
+        <title > Home-Page - Daily Dossier < /title >
+    </head >
+    <body >
+        <h1 > Hi,''' + user['username'] + '''</h1>
+    </body >
+</html> '''
