bb22ab3e0e49b31549a6008e8a49219f57a6d19d
Paul Munyao <paulmunyao094@gmail.com>
2022-02-28 12:23:28 +0300

    Create class article

--- app/models.py
+++ app/models.py
@@ -1,4 +1,19 @@
-class Newsource:
+class source:
     '''
-    New source to define movie Objects
+    class source to define movie Objects
     '''
+
+    def __init__(self,id,title,description):
+        self.id=id
+        self.title=title
+        self.description=description
+
+class article:
+    '''
+    class article to define article
+    '''
+    def __init__(self,id,title,description,author):
+        self.id=id
+        self.title=title
+        self.description=description
+        self.author=author       
