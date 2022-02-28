581e90be6edaa7e9d26558d3b6918c1a0e291bc3
Paul Munyao <paulmunyao094@gmail.com>
2022-02-28 09:23:21 +0300

    Modify files

--- app/request.py
+++ app/request.py
@@ -1,11 +1,15 @@
+import os
+import urllib.request,json
+
 import requests
 from decouple import config
 
 NEWS_API_KEY = config('NEWS_API_KEY')
-COUNTRY = "in"
+COUNTRY = "us"
 
 
 def get_latest_news():
     news_data = requests.get(
         f"https:newsapi.org/v2/top-headlines?country={COUNTRY}&apiKey={NEWS_API_KEY}").json()
     return news_data["articles"]
+
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
 from flask import render_template
 from app import app
+import urllib.request,json
+from .request import get_latest_news
 
 
 @app.route('/')
@@ -8,6 +10,16 @@ def index():
     user = {"username": "There"}
     posts = {"Welcome to the dossier a one stop place where we believe information should be available to everyone, everywhere and  everytime at no cost"}
     return render_template("index.html", title="Home", user=user, posts=posts)
+
+@app.route('/news')
+def get_news():
+    news_get = "https:newsapi.org/v2/top-headlines?country=in&apiKey=d1e7c9d92bd24e31b41ccdca38b4f31c"
+    response = urllib.request.urlopen(news_get)
+    data = response.read()
+    dict = json.loads(data)
+    return render_template("index.html",news=dict["articles"])    
+
+        
     '''
 <html >
     <head >
