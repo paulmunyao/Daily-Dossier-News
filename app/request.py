72f6e973921733e64c49d2dcdd06c14edfee1544
Paul Munyao <paulmunyao094@gmail.com>
2022-03-01 04:08:51 +0300

    Modify files

--- app/request.py
+++ app/request.py
@@ -9,4 +9,12 @@ def get_latest_news():
         f'https://newsapi.org/v2/top-headlines?country={COUNTRY}&apiKey={NEWS_API_KEY}').json()
     return news_data['articles']
 
+NEWS_API_KEY = config('NEWS_API_KEY')
+COUNTRY = 'in'
+
+def get_latest_sources():
+    news_data = requests.get(
+        f'https://newsapi.org/v2/top-headlines?country={COUNTRY}&apiKey={NEWS_API_KEY}').json()
+    return news_data['sources']
+
 
--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from flask import render_template
 from app import app
 import urllib.request,json
 from .request import get_latest_news
+from .request import get_latest_sources
 
 
 @app.route('/')
@@ -16,4 +17,7 @@ def news_headlines():
     news_articles = get_latest_news()
     return render_template("news.html",news_articles=news_articles)    
 
-
+@app.route('/sources')
+def news_sources():
+    news_sources = get_latest_sources()
+    return render_template("news.html",news_sources=news_sources)
